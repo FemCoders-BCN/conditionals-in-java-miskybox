e5e40c2c636f58155fd27894e8a42598348f2f72
SiGu <miskybox@gmail.com>
2025-02-24 17:48:31 +0100

    First commit

--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Quiz {
     public static void main(String[] args) {
         //Escribe un programa que sea un pequeño juego trivial de acertar preguntas
@@ -37,6 +38,61 @@ public class Quiz {
 
         //Respuestas: 1. b / 2. c / 3. b y d / 4. a
 
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+        System.out.println("\t¡Bienvenid@ al quiz de Mujeres&Tecnología!");
+        System.out.println("\tCada respuesta correcta suma 5 puntos! Buena Suerte!");
+
+       
+        System.out.println("Pregunta 1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?");
+        System.out.println("\ta. Ada Lovelace");
+        System.out.println("\tb. Jude Milhon");
+        System.out.println("\tc. Martha Ackelsberg");
+        System.out.println("\td. Jane Jacobs");
+        String answer1 = scanner.nextLine().trim().toLowerCase();
+        if (answer1.equals("b")) {
+            score += 5;
+        }
+        
+        System.out.println("Pregunta 2. ¿Quién es Fuencisla Clemares?");
+        System.out.println("\ta. La directora general de IBM Europa, Oriente Medio y África");
+        System.out.println("\tb. La vicepresidenta de Microsoft Western Europe");
+        System.out.println("\tc. La directora general de Google en España y Portugal");
+        System.out.println("\td. Fundadora y CEO de Marsi Bionics");
+        String answer2 = scanner.nextLine().trim().toLowerCase();
+        if (answer2.equals("c")) {
+            score += 5;
+        }
+
+     
+        System.out.println(" Pregunta 3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?");
+        System.out.println("\ta. Primera programadora de software");
+        System.out.println("\tb. Una actriz de cine");
+        System.out.println("\tc. Inventora del procesador de datos");
+        System.out.println("\td. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS");
+        String answer3 = scanner.nextLine().trim().toLowerCase();
+        if (answer3.equals("b") || answer3.equals("d")) {
+            score += 5;
+        }
+
+        System.out.println("Pregunta 4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?");
+        System.out.println("\ta. Katherine Johnson, Dorothy Vaughan y Mary Jackson");
+        System.out.println("\tb. Jessica Miller, Emily Johnson, Sarah Davis");
+        System.out.println("\tc. bell hooks, Toni Morrison y Alice Walker");
+        System.out.println("\td. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman");
+        String answer4 = scanner.nextLine().trim().toLowerCase();
+        if (answer4.equals("a")) {
+            score += 5;
+
     }
-    
+        System.out.println("\nTu puntaje es: " + score + "/20");
+        if (score >= 15) {
+            System.out.println("¡Tú si que sabes mucho!");
+        } else if (score >= 5) {
+            System.out.println("¡No lo has hecho mal!");
+        } else {
+            System.out.println("¡Buena suerte la próxima vez!");
+        }
+        scanner.close();
+    }
 }
--- TrafficLights.java
+++ TrafficLights.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
 public class TrafficLights {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el color del semáforo: ");
         //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
         
         //Si es verde imprime en la terminal: Puedes seguir
         //Si es amarillo imprime en la terminal: Ten cuidado
         //Si es rojo imprime en la terminal: ¡¡¡Para!!!
         //Si es otro color imprime en la terminal: El semáforo está dañado
+        String color = scanner.nextLine().toLowerCase();
+
+        if (color.equals("verde")) {
+            System.out.println("\nPuedes seguir");
+        } else if (color.equals("amarillo")) {
+            System.out.println("\nTen cuidado");
+        } else if (color.equals("rojo")) {
+            System.out.println("\n¡¡¡Para!!!");
+        } else {
+            System.out.println("\nEl semáforo está dañado");
+        }
+
 
         //Cambia el valor de la variable, corre el programa y mira el resultado 
+    scanner.close();
 
     }
     
